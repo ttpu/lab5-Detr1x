88bd42a86086103cb219ec5948c4150782091cc3
123 <94508583+Detr1x@users.noreply.github.com>
2024-11-06 06:30:44 +0000

    completed 5th exersize

--- ex5/main.c
+++ ex5/main.c
@@ -1,7 +1,14 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    printf("Lowercase\tASCII\tUppercase\tASCII\n");
+    printf("------------------------------------------------\n");
+    
+    for (int i = 0; i < 26; i++) {
+        char lowercase = 'a' + i;
+        char uppercase = 'A' + i;
+        printf("%c\t\t%d\t%c\t\t%d\n", lowercase, lowercase, uppercase, uppercase);
+    }
+
+    return 0;
 }
